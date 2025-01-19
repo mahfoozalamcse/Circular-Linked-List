8601621d217fc21bc808b1550a6146b6c9cac2b4
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-19 06:47:15 +0000

    completed Basic of circular linked list

--- Circular1.java
+++ Circular1.java
@@ -79,6 +79,52 @@ public class Circular1 {
         }
     }
 
+    // Delete head of the circular linked list
+    public static Node deleteHead(Node head){
+        if (head == null) {
+            return null;
+        }
+        if (head.next == head) {
+            return null;
+        }
+        Node curr = head;
+        while (curr.next != head) 
+              curr = curr.next;
+        curr.next = head.next;
+
+        
+        
+        return curr.next;
+    }
+
+    // Delete head efficient way O(1)
+    public static Node deleteHead2(Node head){
+        if (head == null) {
+            return null;
+        }
+        if (head.next == head) {
+            return null;
+        }
+        head.data = head.next.data;
+        head.next = head.next.next;
+        return head;
+    }
+
+    // Delete Kth node of the Linked List
+    public static Node deletekth(Node head, int k){
+        if (head == null) {
+            return head;
+        }
+        if (k == 1) {
+            return deleteHead2(head);
+        }
+        
+        Node temp = head;
+        for(int i=0; i<k-2; i++)
+            temp = temp.next;
+        temp.next = temp.next.next;
+        return head;
+    }
     // traversal method 1 
     public static void printCirList(Node head){
         if (head == null) {
@@ -117,8 +163,16 @@ public class Circular1 {
     // head = insertEnd1(head, 0);
     // printCirList(head);
 
-    head = insertEnd2(head, 0);
-    printCirList(head);
+    // head = insertEnd2(head, 0);
+    // printCirList(head);
 
+    // head = deleteHead(head);
+    // printCirList(head);
+
+//    head = deleteHead2(head);
+//    printCirList(head);
+
+    head = deletekth(head, 2);
+    printCirList(head);
     }
 }
